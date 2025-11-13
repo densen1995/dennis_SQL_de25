5911f55044b4a9e5ccafb8debaf7cac55d001ce9
Dennis Chinecherem <densenj@outlook.com>
2025-11-13 22:28:57 +0100

    ingets bike.sql command into bike_temp db

---
+++ weekly1_query_bikedata/sql/ingest_bike.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE TABLE IF NOT EXISTS staging.orders AS (
+    SELECT * FROM read_csv_auto('data/orders.csv')
+);
+
+
+
+CREATE TABLE IF NOT EXISTS staging.customers AS (
+    SELECT * FROM read_csv_auto('data/customers.csv')
+);
